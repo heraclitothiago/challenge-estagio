4d303f06562b699fa1d7f1a6026924477d3a2ac3
Heraclito Thiago <heraclitothiago@hotmail.com>
2021-04-03 16:38:47 -0300

    routes created

--- index.php
+++ index.php
@@ -1,6 +1,24 @@
 <?php
 
+ob_start();
+require __DIR__ . "/vendor/autoload.php";
 
-require __DIR__."/vendor/autoload.php";
+
+use CoffeeCode\Router\Router;
+
+$route = new Router(url(), ":");
+$route->namespace("Source\Application");
+
+$route->group(null);
+$route->get("/", "ListPosts:home");
+$route->get("/{userId}", "ListPosts:home");
+$route->get("/{userId}/{id}", "ListPosts:home");
 
 
+$route->dispatch();
+
+if ($route->error()) {
+    $route->redirect(url());
+}
+
+ob_end_flush();
