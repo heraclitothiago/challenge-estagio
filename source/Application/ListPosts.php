b4c2e54c9d2d8e597e1325bc6e1498f05be6647b
Heraclito Thiago <heraclitothiago@hotmail.com>
2021-04-03 17:16:55 -0300

    home method refactored

--- source/Application/ListPosts.php
+++ source/Application/ListPosts.php
@@ -4,7 +4,23 @@
 namespace Source\Application;
 
 
-class ListPosts
+class ListPosts extends Controller
 {
+    public function __construct()
+    {
+        parent::__construct(__DIR__ . "/../../theme/app/");
+    }
 
+    public function home()
+    {
+        $params = null;
+        echo $this->view->render("view1",[
+            "params" => $params
+        ]);
+    }
+
+    public function error()
+    {
+
+    }
 }
