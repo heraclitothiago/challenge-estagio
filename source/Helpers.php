dca0d87cc12bcf01f8648fb29d0ddec689c57410
Heraclito Thiago <heraclitothiago@hotmail.com>
2021-04-03 17:33:53 -0300

    method  theme on Helpers created

--- source/Helpers.php
+++ source/Helpers.php
@@ -11,3 +11,17 @@ function url(string $path = null): string
     }
     return CONF_URL;
 }
+
+
+/**
+ * @param string|null $path
+ * @return string
+ */
+function theme(string $path = null): string
+{
+    if ($path) {
+        return CONF_URL . "/theme/app/" . ($path[0] == "/" ? mb_substr($path, 1) : $path);
+    }
+
+    return CONF_URL . "/themes/app/";
+}
