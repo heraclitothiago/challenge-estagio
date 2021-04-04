906dfe3af8b7c86db6f4014b5bc58730b2ff17e7
Heraclito Thiago <heraclitothiago@hotmail.com>
2021-04-04 10:23:36 -0300

    view1 as home / listing posts

--- theme/app/view1.php
+++ theme/app/view1.php
@@ -1,5 +1,27 @@
 <?php $v->layout("_theme"); ?>
 
-<div class="container">
-    <h1>Olá Mundo</h1>
+<div class="album py-5 bg-light">
+    <div class="container">
+        <div class="row">
+            <?php
+            foreach ($posts as $post) {
+                echo <<<POST
+<div class="col-md-4">
+                <div class="card mb-4 shadow-sm">
+                    <div class="card-body">
+                        <h5 class="card-title">{$post['title']}</h5>
+                        <p class="card-text">{$post['body']}</p>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <a href="#" class="card-link">Card link</a>
+                            <small class="text-muted">por userId {$post['userId']}</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+POST;
+            }
+            ?>
+
+        </div>
+    </div>
 </div>
