e10146c036ae7721f2eb5072f4dedc8be2402ad0
Heraclito Thiago <heraclitothiago@hotmail.com>
2021-04-04 13:09:05 -0300

    view1 refactored

--- theme/app/view1.php
+++ theme/app/view1.php
@@ -5,20 +5,21 @@
         <div class="row">
             <?php
             foreach ($posts as $post) {
-                echo <<<POST
-<div class="col-md-4">
+                echo '
+                <div class="col-md-4">
                 <div class="card mb-4 shadow-sm">
                     <div class="card-body">
-                        <h5 class="card-title">{$post['title']}</h5>
-                        <p class="card-text">{$post['body']}</p>
+                        <h5 class="card-title">' . str_limit_chars(str_title($post["title"]), 25) . '</h5>
+                        <p class="card-text">' . str_limit_chars($post["body"], 115) . '</p>
                         <div class="d-flex justify-content-between align-items-center">
                             <a href="#" class="card-link">Card link</a>
-                            <small class="text-muted">por userId {$post['userId']}</small>
-                        </div>
-                    </div>
-                </div>
-            </div>
-POST;
+                            <small class="text-muted">por userId ' .
+                    $post["userId"] . '</small >
+                        </div >
+                    </div >
+                </div >
+            </div >
+    ';
             }
             ?>
         </div>
