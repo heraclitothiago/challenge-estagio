58128f3fa993063716227ddceaeb42a285d0b5bb
Heraclito Thiago <heraclitothiago@hotmail.com>
2021-04-04 10:44:45 -0300

    string helpers created

--- source/Helpers.php
+++ source/Helpers.php
@@ -1,6 +1,71 @@
 <?php
 
 /**
+ * @param string $string
+ * @return string
+ */
+function str_slug(string $string): string
+{
+    $string = filter_var(mb_strtolower($string), FILTER_SANITIZE_STRIPPED);
+    $formats = 'ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜüÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûýýþÿRr"!@#$%&*()_-+={[}]/?;:.,\\\'<>°ºª';
+    $replace = 'aaaaaaaceeeeiiiidnoooooouuuuuybsaaaaaaaceeeeiiiidnoooooouuuyybyRr                                 ';
+
+    $slug = str_replace(["-----", "----", "---", "--"], "-",
+        str_replace(" ", "-",
+            trim(strtr(utf8_decode($string), utf8_decode($formats), $replace))
+        )
+    );
+    return $slug;
+}
+
+/**
+ * @param string $string
+ * @return string
+ */
+function str_title(string $string): string
+{
+    return mb_convert_case(filter_var($string, FILTER_SANITIZE_SPECIAL_CHARS), MB_CASE_TITLE);
+}
+
+/**
+ * @param string $string
+ * @param int $limit
+ * @param string $pointer
+ * @return string
+ */
+function str_limit_words(string $string, int $limit, string $pointer = "..."): string
+{
+    $string = trim(filter_var($string, FILTER_SANITIZE_SPECIAL_CHARS));
+    $arrWords = explode(" ", $string);
+    $numWords = count($arrWords);
+
+    if ($numWords < $limit) {
+        return $string;
+    }
+
+    $words = implode(" ", array_slice($arrWords, 0, $limit));
+    return "{$words}{$pointer}";
+}
+
+/**
+ * @param string $string
+ * @param int $limit
+ * @param string $pointer
+ * @return string
+ */
+function str_limit_chars(string $string, int $limit, string $pointer = "..."): string
+{
+    $string = trim(filter_var($string, FILTER_SANITIZE_SPECIAL_CHARS));
+    if (mb_strlen($string) <= $limit) {
+        return $string;
+    }
+
+    $chars = mb_substr($string, 0, mb_strrpos(mb_substr($string, 0, $limit), " "));
+    return "{$chars}{$pointer}";
+}
+
+
+/**
  * @param string|null $path
  * @return string
  */
