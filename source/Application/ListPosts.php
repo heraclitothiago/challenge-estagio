110b8c274615c3a056c8b5177a4a1022f1b1f20c
Heraclito Thiago <heraclitothiago@hotmail.com>
2021-04-04 13:09:43 -0300

    almost done methode home to list posts

--- source/Application/ListPosts.php
+++ source/Application/ListPosts.php
@@ -11,16 +11,36 @@ class ListPosts extends Controller
         parent::__construct(__DIR__ . "/../../theme/app/");
     }
 
-    public function home()
+    public function home(array $data): void
+    {
+        //load json and covert to assoc array
+        $postList = file_get_contents(CONF_API_ADDRESS);
+        $postCollection = json_decode($postList, true);
+
+        //create pagination
+        $pagination = new Pager(url('/post/p/'));
+        $pagination->pager(count($postCollection), 9, ($data['page'] ?? 1));
+
+        //render page
+        echo $this->view->render("view1", [
+            "posts" => $postCollection,
+            "pagination" => $pagination->render('')
+        ]);
+    }
+
+    public function posts()
     {
         $params = null;
-        echo $this->view->render("view1",[
+        echo $this->view->render("view2", [
             "params" => $params
         ]);
     }
 
     public function error()
     {
-
+        $params = 'null';
+        echo $this->view->render("view2", [
+            "params" => $params
+        ]);
     }
 }
