c07aa8896f2955b99a1becc1857cc70a9ac55a02
Heraclito Thiago <heraclitothiago@hotmail.com>
2021-04-04 14:21:32 -0300

    Page view2 created

--- theme/app/view2.php
+++ theme/app/view2.php
@@ -1,2 +1,13 @@
 <?php $v->layout("_theme"); ?>
 
+<div class="container">
+    <div class="col-md-8 blog-main">
+        <div class="blog-post">
+            <h2><?= 'HEAD'; ?></h2>
+            <p style="margin-bottom: 1.25rem; color: #999">January 1, 2014 by <a href="#">Mark</a></p>
+            <p>Suiting up for my crowning battle. If you only knew what the future holds. Bring the beat back.
+                Peach-pink
+                lips, yeah, everybody stares.</p>
+        </div>
+    </div>
+</div>
