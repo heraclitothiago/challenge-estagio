ce25a6082df0d742cbe164fd66528b9dfad472bf
Heraclito Thiago <heraclitothiago@hotmail.com>
2021-04-04 16:28:41 -0300

    method to see single post was finished successfully

--- source/Application/ModelEmulate.php
+++ source/Application/ModelEmulate.php
@@ -25,19 +25,20 @@ class ModelEmulate
         return $postCollection;
     }
 
-    /**
-     * @param int $userId
-     * @param int $postId
-     * @return mixed|string
-     */
-    public function getPost(int $userId, int $postId){
-        if (!isset($userId) || !isset($postId)){
+
+    public function getPost(int $userId, int $postId)
+    {
+        if (!isset($userId) || !isset($postId)) {
             return "Informe o UserId e o id do Post";
         }
-        if (!is_int($userId) || !is_int($postId)){
+        if (!is_int($userId) || !is_int($postId)) {
             return "UserId e Post Id devem ser números inteiros";
         }
-        return $this->getAllPosts($userId)[$postId - 1];
+        $url = CONF_API_ADDRESS . "?postId={$userId}&id={$postId}";
+        $post = file_get_contents($url);
+        $postDecoded = json_decode($post, true);
+        $post = $postDecoded[0];
+        return $post;
     }
 
 }
